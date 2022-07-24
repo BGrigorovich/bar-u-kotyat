fc77769b4db3e89257168b95bd841e97d5f770c9
Bohdan <bohdan.hryhorovych@developex.net>
2022-07-24 22:18:59 +0200

    First commit

--- bar_u_kotyat/bar_u_kotyat_stack.py
+++ bar_u_kotyat/bar_u_kotyat_stack.py
@@ -1,19 +1,40 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_events as events,
+    aws_lambda as lambda_,
+    aws_events_targets as targets,
+    aws_dynamodb as dynamodb, Duration
 )
+
 from constructs import Construct
+
 
 class BarUKotyatStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        cocktails_table_name = 'Cocktails'
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "BarUKotyatQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        with open('update-menu.py', encoding='utf8') as fp:
+            handler_code = fp.read()
+
+        lambda_fn = lambda_.Function(
+            self,
+            'RandomWriter',
+            code=lambda_.InlineCode(handler_code),
+            handler='index.main',
+            timeout=Duration.seconds(10),
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            environment={'TABLE_NAME': cocktails_table_name},
+        )
+
+        dynamodb_cocktails_table = dynamodb.Table(
+            self,
+            cocktails_table_name,
+            table_name=cocktails_table_name,
+            partition_key=dynamodb.Attribute(name='id', type=dynamodb.AttributeType.String),
+            billing_mode=dynamodb.BillingMode.PayPerRequest)
+        dynamodb_cocktails_table.grant_stream_read(lambda_fn)
+        dynamodb_cocktails_table.grant_read_data(lambda_fn)
