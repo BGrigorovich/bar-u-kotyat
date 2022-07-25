9c6315f63f54ccf34330332d99340b84607c6a4f
Bohdan <bohdan.hryhorovych@developex.net>
2022-07-25 17:06:43 +0200

    Add S3 bucket, add dynamodb event source for lambda

--- bar_u_kotyat/bar_u_kotyat_stack.py
+++ bar_u_kotyat/bar_u_kotyat_stack.py
@@ -1,10 +1,10 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    aws_events as events,
     aws_lambda as lambda_,
-    aws_events_targets as targets,
-    aws_dynamodb as dynamodb, Duration
+    aws_dynamodb as dynamodb, Duration,
+    aws_s3 as s3,
+    aws_s3_deployment as s3_deployment,
+    aws_lambda_event_sources as lambda_event_sources
 )
 
 from constructs import Construct
@@ -17,26 +17,49 @@ class BarUKotyatStack(Stack):
 
         cocktails_table_name = 'Cocktails'
 
-        with open('bar_u_kotyat/update-menu.py', encoding='utf8') as fp:
-            handler_code = fp.read()
-
-        lambda_fn = lambda_.Function(
-            self,
-            'RandomWriter',
-            code=lambda_.InlineCode(handler_code),
-            handler='index.main',
-            timeout=Duration.seconds(10),
-            runtime=lambda_.Runtime.PYTHON_3_9,
-            environment={'TABLE_NAME': cocktails_table_name},
-        )
+        with open('bar_u_kotyat/update-menu.py', encoding='utf8') as lambda_source:
+            lambda_fn = lambda_.Function(
+                self,
+                'RenderMenu',
+                code=lambda_.InlineCode(lambda_source.read()),
+                handler='index.main',
+                timeout=Duration.seconds(10),
+                runtime=lambda_.Runtime.PYTHON_3_9,
+                environment={'TABLE_NAME': cocktails_table_name},
+            )
 
         dynamodb_cocktails_table = dynamodb.Table(
             self,
             cocktails_table_name,
             table_name=cocktails_table_name,
             partition_key=dynamodb.Attribute(name='id', type=dynamodb.AttributeType.STRING),
-            # TODO: set RCU
-            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST)
-        # todo: enable streams
-        # dynamodb_cocktails_table.grant_stream_read(lambda_fn)
+            stream=dynamodb.StreamViewType.KEYS_ONLY,
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
+        dynamodb_cocktails_table.grant_stream_read(lambda_fn)
         dynamodb_cocktails_table.grant_read_data(lambda_fn)
+
+        lambda_fn.add_event_source(
+            lambda_event_sources.DynamoEventSource(
+                table=dynamodb_cocktails_table,
+                starting_position=lambda_.StartingPosition.LATEST,
+                enabled=True
+            )
+        )
+
+        bucket = s3.Bucket(
+            self,
+            'BarUKotyat',
+            versioned=True,
+            public_read_access=True,
+            website_index_document='menu.html'
+        )
+        bucket.grant_read_write(lambda_fn)
+
+        with open('bar_u_kotyat/menu_template.html', encoding='utf8') as menu_template:
+            s3_deployment.BucketDeployment(
+                self,
+                'Deploy menu_template.html',
+                destination_bucket=bucket,
+                sources=[s3_deployment.Source.data('menu_template.html', menu_template.read())]
+            )
