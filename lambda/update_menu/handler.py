c8b335a3fc9d65298c71b08741ad0300fd34c272
Bohdan <bohdan.hryhorovych@developex.net>
2022-08-14 16:20:37 +0200

    Add order and available fields to cocktails

--- lambda/update_menu/handler.py
+++ lambda/update_menu/handler.py
@@ -1,7 +1,10 @@
 import os
-from collections import defaultdict
+from dataclasses import dataclass, field, replace
+from math import inf
+from typing import List
 
 import boto3
+from dynamodb_json import json_util as dynamodb_json
 from jinja2 import Environment, select_autoescape
 
 
@@ -11,6 +14,27 @@ table_name = os.getenv('TABLE_NAME')
 bucket_name = os.getenv('BUCKET_NAME')
 
 jinja_env = Environment(autoescape=select_autoescape())
+
+
+@dataclass
+class Cocktail:
+    name: str
+    ingredients: List[str]
+    order: int = inf
+    available: bool = True
+    notes: str = ''
+
+    @property
+    def ingredients_str(self):
+        return ', '.join(self.ingredients).capitalize()
+
+
+@dataclass
+class CocktailGroup:
+    name: str
+    order: int = inf
+    available: bool = True
+    cocktails: List[Cocktail] = field(default_factory=lambda: [])
 
 
 def lambda_handler(event, context):
@@ -27,18 +51,32 @@ def lambda_handler(event, context):
     for page in page_iterator:
         cocktails.extend(page['Items'])
 
-    cocktail_groups = defaultdict(list)
+    cocktail_groups = []
     for cocktail in cocktails:
-        if cocktail['sk']['S'].startswith('cocktail#'):
-            group_name = cocktail['pk']['S']
-            ingredients = [ing['S'] for ing in cocktail['ingredients']['L']]
-            cocktail_groups[group_name].append({
-                'name': cocktail['sk']['S'].split('#')[1],
-                'ingredients': ', '.join(ingredients).capitalize()
-            })
-            cocktail['name'] = cocktail['sk']['S'].split('#')[1]
-            cocktail['ingredients'] = ', '.join(ingredients).capitalize()
+        cocktail = dynamodb_json.loads(cocktail)
 
+        group_name = cocktail.pop('pk')
+        try:
+            cocktail_group = next(group for group in cocktail_groups if group.name == group_name)
+        except StopIteration:
+            cocktail_group = CocktailGroup(name=group_name)
+            cocktail_groups.append(cocktail_group)
+
+        if cocktail['sk'].startswith('cocktail#'):
+            cocktail['name'] = cocktail.pop('sk').replace('cocktail#', '')
+            cocktail = Cocktail(**cocktail)
+            if cocktail.available:
+                cocktail_group.cocktails.append(cocktail)
+        else:
+            cocktail_group.order = cocktail.get('order', inf)
+            cocktail_group.available = cocktail.get('available', True)
+
+    cocktail_groups = [
+        cocktail_group for cocktail_group in sorted(cocktail_groups, key=lambda group: group.order)
+        if cocktail_group.available
+    ]
+    for cocktail_group in cocktail_groups:
+        cocktail_group.cocktails.sort(key=lambda c: c.order)
     html_context = dict(
         cocktail_groups=cocktail_groups,
     )
